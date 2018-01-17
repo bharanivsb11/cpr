45f90af54fb40db141ebc1a1c7cef6502c67d87d
bharanivsb11 <35220778+bharanivsb11@users.noreply.github.com>
2018-01-17 16:52:11 +0530

    alphabet.C

---
+++ alphabet.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("enter the character");
+scanf("%c",&ch);
+if(ch=='a'&&ch<='z')
+{
+printf("given character is an alphabet");
+}
+else
+{
+printf("given character is not an alaphabet");
+}
+return 0;
+}
